98e5ba9fa377d135f334efe0436c41506c1402c1
Fabio Miranda <web.jrhk@gmail.com>
2024-04-22 15:55:03 -0300

    Created and implemented the class StatusChangedEventArgs

---
+++ ChatServer/StatusChangedEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+    // Trata os argumentos para o evento StartChanged
+
+    internal class StatusChangedEventArgs : EventArgs
+    {
+        private String EventMsg;
+
+        public String EventMessage
+        { 
+            get { return EventMsg; }
+            set { EventMsg = value; }
+        }
+
+        public StatusChangedEventArgs(string strEventMsg) 
+        { 
+            EventMsg = strEventMsg;
+        }
+    }
+}
